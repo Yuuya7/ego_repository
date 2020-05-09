70afeeb6b206db31b1666be815a2b798498391be
Yuuya7 <472259599@qq.com>
2020-05-09 16:14:43 +0800

    第三次提交

--- ego-project11/ego-base/src/main/java/cn/cloud/ego/base/vo/Test.java
+++ ego-project11/ego-base/src/main/java/cn/cloud/ego/base/vo/Test.java
@@ -3,7 +3,7 @@ package cn.cloud.ego.base.vo;
 public class Test {
 
     public static void main(String[] args){
-        System.out.println("Test");
+        System.out.println("Test123456");
     }
 
 }
